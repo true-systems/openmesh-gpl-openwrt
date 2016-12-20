96919eca1835f4ce84302f818a16e12f7a5dc98e
Petr Štetiar <ynezz@true.cz>
2016-12-20 11:37:38 +0100

    Adding sources as recevied from OpenMesh
    I've just deleted the content of dl directory (downloads).
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

---
+++ include/kernel-version.mk
@@ -0,0 +1,33 @@
+# Use the default kernel version if the Makefile doesn't override it
+
+LINUX_RELEASE?=1
+
+LINUX_VERSION-3.8  = .13
+LINUX_VERSION-3.10 = .58
+LINUX_VERSION-3.13 = .7
+LINUX_VERSION-3.14 = .34
+LINUX_VERSION-3.18 = .8
+LINUX_VERSION-3.19 = 
+LINUX_VERSION-4.0 = -rc1
+
+LINUX_KERNEL_MD5SUM-3.8.13  = 2af19d06cd47ec459519159cdd10542d
+LINUX_KERNEL_MD5SUM-3.10.58 = 3ff3478b6351143cef22d4b81cf48b01
+LINUX_KERNEL_MD5SUM-3.13.7  = 370adced5e5c1cb1d0d621c2dae2723f
+LINUX_KERNEL_MD5SUM-3.14.34 = 4061154fb66b24a8a1982a8adbe94335
+LINUX_KERNEL_MD5SUM-3.18.8  = 94673d5f4521a6077eb6fe0ab2633b35
+LINUX_KERNEL_MD5SUM-3.19    = d3fc8316d4d4d04b65cbc2d70799e763
+LINUX_KERNEL_MD5SUM-4.0-rc1 = 547b340dca94d358b68d2658822080fa
+
+ifdef KERNEL_PATCHVER
+  LINUX_VERSION:=$(KERNEL_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_PATCHVER)))
+endif
+
+split_version=$(subst ., ,$(1))
+merge_version=$(subst $(space),.,$(1))
+KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
+KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
+KERNEL_PATCHVER ?= $(KERNEL)
+
+# disable the md5sum check for unknown kernel versions
+LINUX_KERNEL_MD5SUM:=$(LINUX_KERNEL_MD5SUM-$(strip $(LINUX_VERSION)))
+LINUX_KERNEL_MD5SUM?=x
---
+++ target/linux/octeon/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,89 @@
+#
+# Copyright (C) 2014 OpenWrt.org
+#
+
+. /lib/functions/octeon.sh
+
+platform_get_rootfs() {
+	local rootfsdev
+
+	if read cmdline < /proc/cmdline; then
+		case "$cmdline" in
+			*block2mtd=*)
+				rootfsdev="${cmdline##*block2mtd=}"
+				rootfsdev="${rootfsdev%%,*}"
+			;;
+			*root=*)
+				rootfsdev="${cmdline##*root=}"
+				rootfsdev="${rootfsdev%% *}"
+			;;
+		esac
+
+		echo "${rootfsdev}"
+	fi
+}
+
+platform_copy_config() {
+	local board="$(octeon_board_name)"
+
+	case "$board" in
+	erlite)
+		mount -t vfat /dev/sda1 /mnt
+		cp -af "$CONF_TAR" /mnt/
+		umount /mnt
+		;;
+	esac
+}
+
+platform_do_upgrade() {
+	local board=$(octeon_board_name)
+	local rootfs="$(platform_get_rootfs)"
+
+	[ -b "${rootfs}" ] || return 1
+
+	case "$board" in
+	erlite)
+		local tar_file="$1"
+		local kernel_length=`(tar xf $tar_file sysupgrade-erlite/kernel -O | wc -c) 2> /dev/null`
+		local rootfs_length=`(tar xf $tar_file sysupgrade-erlite/root -O | wc -c) 2> /dev/null`
+
+		mkdir -p /boot
+		mount -t vfat /dev/sda1 /boot
+
+		[ -f /boot/vmlinux.64 -a ! -L /boot/vmlinux.64 ] && {
+			mv /boot/vmlinux.64 /boot/vmlinux.64.previous
+			mv /boot/vmlinux.64.md5 /boot/vmlinux.64.md5.previous
+		}
+
+		tar xf $tar_file sysupgrade-erlite/kernel -O > /boot/vmlinux.64
+		md5sum /boot/vmlinux.64 | cut -f1 -d " " > /boot/vmlinux.64.md5
+		tar xf $tar_file sysupgrade-erlite/root -O | dd of="${rootfs}" bs=4096
+		sync
+		umount /boot
+		return 0
+		;;
+	esac
+
+	return 1
+	
+}
+
+platform_check_image() {
+	local board=$(octeon_board_name)
+
+	case "$board" in
+	erlite)
+		local tar_file="$1"
+		local kernel_length=`(tar xf $tar_file sysupgrade-erlite/kernel -O | wc -c) 2> /dev/null`
+		local rootfs_length=`(tar xf $tar_file sysupgrade-erlite/root -O | wc -c) 2> /dev/null`
+		[ "$kernel_length" = 0 -o "$rootfs_length" = 0 ] && {
+			echo "The upgarde image is corrupt."
+			return 1
+		}
+		return 0
+		;;
+	esac
+
+	echo "Sysupgrade is not yet supported on $board."
+	return 1
+}
